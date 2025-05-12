5b65d4d046f9207bba674df5972f0d6c43048b19
Zuhriddin <javohirruzimurodov608@gmail.com>
2025-05-12 09:01:36 +0500

    asdfsdf

--- js/main.js
+++ js/main.js
@@ -6,3 +6,10 @@ let body = document.querySelector("body");
 darkBtn.addEventListener("click", () => {
   body.classList.toggle("dark");
 });
+
+let checkbox = document.querySelector("#checkbox");
+let nav = document.querySelector("nav");
+
+checkbox.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
